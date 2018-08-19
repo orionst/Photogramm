ec89ccc36ca3e5b71610785c869290645774f0ee
Viktor <vm.orion@gmail.com>
2018-08-19 21:50:49 +0300

    First commit

---
+++ app/src/main/java/com/orionst/mymaterialdesignapp/ThemeChoosingActivity.java
@@ -0,0 +1,13 @@
+package com.orionst.mymaterialdesignapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThemeChoosingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_choosing);
+    }
+}
