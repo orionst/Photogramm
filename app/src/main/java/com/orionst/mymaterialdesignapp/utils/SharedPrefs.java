77181594a20764c00ea469683b3a4e59e093dce0
Viktor <vm.orion@gmail.com>
2018-08-23 15:24:42 +0300

    * Основной экран и экран выбора темы реализованы через фрагменты. + Добавлен Navigation Drawer. + Добавлена кнопка FAB, по которой открывается системная камера. Сделанный   снимок записывается в каталог приложения в папку Pictures. + Для вывода списка фотографий на основном экране создан RecyclerView, но   сам вывод не реализован. + На основной экран для целей тестирования добавлен ImageView, куда   передается сфотографированное изображение, потом уберу. * Прочие мелкие изменения.

--- app/src/main/java/com/orionst/mymaterialdesignapp/utils/SharedPrefs.java
+++ app/src/main/java/com/orionst/mymaterialdesignapp/utils/SharedPrefs.java
@@ -1,22 +1,28 @@
 package com.orionst.mymaterialdesignapp.utils;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class SharedPrefs {
+import com.orionst.mymaterialdesignapp.R;
+
+public final class SharedPrefs {
+
+    public static final String PREFS_NAME = "MyMaterialDesign";
+
+    public static final String CURRENT_THEME = "b97517e4-7653-4f08-b813-232bce45e08a";
+    public static final int DEFAULT_THEME = R.style.AppThemeWarm;
 
     private SharedPrefs() {}
 
     public static void saveCurrentTheme(Context context, int placeObjStr) {
-        SharedPreferences.Editor editor = context.getSharedPreferences(Constants.PREFS_NAME, Activity.MODE_PRIVATE).edit();
-        editor.putInt(Constants.CURRENT_THEME, placeObjStr);
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit();
+        editor.putInt(CURRENT_THEME, placeObjStr);
         editor.apply();
     }
 
     public static int getCurrentTheme(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.PREFS_NAME, Activity.MODE_PRIVATE);
-        return sharedPreferences.getInt(Constants.CURRENT_THEME, Constants.DEFAULT_THEME);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getInt(CURRENT_THEME, DEFAULT_THEME);
     }
 
 }
